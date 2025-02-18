7421af85e4e07ced82211fd5d91e9a5f4a00cba7
Adrian Robles <adrianrobles2706@gmail.com>
2025-02-18 22:33:06 +0100

    Comprobar que el IoC no existe ya en la base de datos

--- crud.py
+++ crud.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from models import IoC
@@ -10,6 +11,14 @@ async def get_iocs(db: AsyncSession):
 
 #Función para crear IoCs
 async def create_ioc(db: AsyncSession, ioc_data: IoCCreate):
+    # Verificar si el IoC ya existe en la base de datos
+    stmt = select(IoC).where(IoC.valor == ioc_data.valor)
+    result = await db.execute(stmt)
+    existing_ioc = result.scalars().first()
+
+    if existing_ioc:
+        raise HTTPException(status_code=400, detail="El IoC ya existe en la base de datos.")
+    
     new_ioc = IoC(**ioc_data.dict())
     db.add(new_ioc)
     await db.commit()
